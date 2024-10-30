0a02906f8f59466a69852cb484aec7cc6ecfc450
anzuukino <songokult1@gmail.com>
2024-10-30 23:35:13 +0700

    test

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args){
+
+
+    }
+    public static void writeFile
+    private static void clearConsole() {
+        System.out.print("\033[H\033[2J"); // ANSI escape code to clear screen and reset cursor
+        System.out.flush();
+    }
+}
